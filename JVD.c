6cf84c67f51d87fc75824339c6372a24dd39ef0a
Matheussenaipudolsk <0001181787@senaimgaluno.com.br>
2025-05-08 19:23:56 -0300

    teste2

--- JVD.c
+++ JVD.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 char tabuleiro[3][3];
 char player = 'X';
+
 
 void inicializarTabuleiro() {
     char pos = '1';
@@ -11,8 +13,9 @@ void inicializarTabuleiro() {
             tabuleiro[i][j] = pos++;
 }
 
-void Tabuleiro() {
-    system("cls");
+
+void exibirTabuleiro() {
+    limparTela();
     printf("\n      1   2   3\n\n");
     for (int i = 0; i < 3; i++) {
         printf(" %d    %c | %c | %c \n", i + 1, tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2]);
@@ -22,15 +25,25 @@ void Tabuleiro() {
     printf("\n");
 }
 
-int verificarVitoria() {
+
+int linhasOuColunasIguais() {
     for (int i = 0; i < 3; i++) {
         if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) return 1;
         if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) return 1;
     }
+    return 0;
+}
+
+int diagonaisIguais() {
     if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) return 1;
     if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) return 1;
     return 0;
 }
+
+int verificarVitoria() {
+    return linhasOuColunasIguais() || diagonaisIguais();
+}
+
 
 int verificarEmpate() {
     for (int i = 0; i < 3; i++)
@@ -40,42 +53,47 @@ int verificarEmpate() {
     return 1;
 }
 
+
 void instrucoes() {
-    system("cls");
+    limparTela(); 
     printf("=== INSTRUCOES DO JOGO ===\n\n");
     printf("1. Dois jogadores alternam jogadas.\n");
     printf("2. O jogador escolhe uma posicao de 1 a 9.\n");
     printf("3. Vence quem formar uma linha, coluna ou diagonal.\n");
     printf("4. Se todas as posicoes forem preenchidas e ninguem vencer, da empate.\n\n");
-    system("pause");
+    pausar();
 }
+
 
 void jogarTurno() {
     char escolha;
     int linha, coluna;
-
-    Tabuleiro();
-    printf("\n Jogador %c, escolha uma posicao (1-9): ", player);
-    scanf(" %c", &escolha);
-
     int valido = 0;
-    for (linha = 0; linha < 3; linha++) {
-        for (coluna = 0; coluna < 3; coluna++) {
-            if (tabuleiro[linha][coluna] == escolha) {
-                tabuleiro[linha][coluna] = player;
-                valido = 1;
-                break;
-            }
-        }
-        if (valido) break;
-    }
 
-    if (!valido) {
-        printf("Posicao invalida ou casa ocupada. Escolha outra.\n");
-        system("pause");
-        jogarTurno();
-    }
+    do {
+        exibirTabuleiro();
+        printf("\n Jogador %c, escolha uma posicao (1-9): ", player);
+        scanf(" %c", &escolha);
+
+        for (linha = 0; linha < 3; linha++) {
+            for (coluna = 0; coluna < 3; coluna++) {
+                if (tabuleiro[linha][coluna] == escolha) {
+                    tabuleiro[linha][coluna] = player;
+                    valido = 1;
+                    break;
+                }
+            }
+            if (valido) break;
+        }
+
+        if (!valido) {
+            printf("Posicao invalida ou casa ocupada. Escolha outra.\n");
+            pausar();
+        }
+
+    } while (!valido);
 }
+
 
 void jogar() {
     player = 'X';
@@ -85,13 +103,13 @@ void jogar() {
         jogarTurno();
 
         if (verificarVitoria()) {
-            Tabuleiro();
+            exibirTabuleiro();
             printf("Parabens! Jogador %c venceu!\n", player);
             break;
         }
 
         if (verificarEmpate()) {
-            Tabuleiro();
+            exibirTabuleiro();
             printf("Empate! Ninguem venceu.\n");
             break;
         }
@@ -100,20 +118,42 @@ void jogar() {
     }
 
     printf("\nFim de jogo!\n");
-    system("pause");
+    pausar();
 }
+
+
+void limparTela() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void pausar() {
+    printf("Pressione Enter para continuar...");
+    getchar();
+    getchar();
+}
+
 
 int main() {
     int opcao;
 
     do {
-        system("cls");
+        limparTela();
         printf("=== MENU PRINCIPAL ===\n");
         printf("1. Jogar\n");
         printf("2. Instrucoes\n");
         printf("3. Sair\n");
         printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
+
+        if (scanf("%d", &opcao) != 1) {
+            while (getchar() != '\n'); 
+            printf("Entrada invalida! Use apenas numeros.\n");
+            pausar();
+            continue;
+        }
 
         switch (opcao) {
             case 1:
@@ -127,7 +167,7 @@ int main() {
                 break;
             default:
                 printf("Opcao invalida! Tente novamente.\n");
-                system("pause");
+                pausar();
         }
     } while (opcao != 3);
 
